17df33c2fc836e003bf4593a2232df505d837df3
Charlie Marlière <charlie.marliere@gmail.com>
2023-11-17 14:16:38 +0100

    trame utilisateurs

---
+++ package/utilisateur.py
@@ -0,0 +1,21 @@
+class Utilisateur:
+    def __init__(set, id_utilisateur, email, nom, prenom, mdp, role_id):
+        set.id_utilisateur=id_utilisateur
+        set.email=email
+        set.nom=nom
+        set.prenom=prenom
+        set.mdp=mdp
+        set.role_id=role_id
+    
+    # methodes
+    def login():
+    def create_user():
+    def write_comment():
+    def like():
+    def write_article():
+    def logout():
+
+    
+        
+    
+    
